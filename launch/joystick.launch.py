3e9862a26e86c9c4646af46b2266c9b9a47826c6
atiyeh2016 <atiyeh.karimipour@gmail.com>
2023-04-11 09:11:43 +0330

    nav2

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -6,6 +6,7 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+
     joy_params = os.path.join(get_package_share_directory('my_bot'), 'config', 'joystick.yaml')
 
     joy_node = Node(
@@ -18,9 +19,9 @@ def generate_launch_description():
     teleop_node = Node(
             package = 'teleop_twist_joy',
             executable='teleop_node',
-            executable='teleop_node',
+            name='teleop_node',
             parameters=[joy_params],
-            remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]
+            remappings=[('/cmd_vel','/cmd_vel_joy')]
         )
     
 
